f04fc47b4af3f7d3a4487a1ef696bdda29c723d1
helen-cho <hd9536@daum.net>
2024-08-29 13:17:53 +0900

    지도출력

--- ex04/app/src/main/java/com/example/ex04/LocalActivity.java
+++ ex04/app/src/main/java/com/example/ex04/LocalActivity.java
@@ -1,13 +1,18 @@
 package com.example.ex04;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -22,6 +27,7 @@ import org.json.JSONObject;
 public class LocalActivity extends AppCompatActivity {
     JSONArray array = new JSONArray();
     LocalAdapter adapter=new LocalAdapter();
+    String query="가산디지털";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,7 +51,7 @@ public class LocalActivity extends AppCompatActivity {
     class LocalThread extends AsyncTask<String, String, String>{
         @Override
         protected String doInBackground(String... strings) {
-            String url="https://dapi.kakao.com/v2/local/search/keyword.json?query=롯데리아";
+            String url="https://dapi.kakao.com/v2/local/search/keyword.json?query=" + query;
             String result=KakaoAPI.connect(url);
             return result;
         }
@@ -82,7 +88,55 @@ public class LocalActivity extends AppCompatActivity {
         @Override
         public View getView(int i, View view, ViewGroup viewGroup) {
             View item = getLayoutInflater().inflate(R.layout.item_local, viewGroup, false);
+            try{
+                JSONObject obj = array.getJSONObject(i);
+                String strName = obj.getString("place_name");
+                String strAddress = obj.getString("address_name");
+                String strPhone = obj.getString("phone");
+                String strX = obj.getString("x");
+                String strY = obj.getString("y");
+                TextView name=item.findViewById(R.id.name);
+                name.setText(strName);
+                TextView address=item.findViewById(R.id.address);
+                address.setText(strAddress);
+                TextView phone=item.findViewById(R.id.phone);
+                phone.setText(strPhone);
+                ImageView local=item.findViewById(R.id.local);
+                local.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent =new Intent(LocalActivity.this, MapsActivity.class);
+                        intent.putExtra("x", strX);
+                        intent.putExtra("y", strY);
+                        intent.putExtra("name", strName);
+                        startActivity(intent);
+                    }
+                });
+            }catch (Exception e){
+                Log.e("getView", e.toString());
+            }
             return item;
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        SearchView searchView=(SearchView)menu.findItem(R.id.search).getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                query=s;
+                new LocalThread().execute();
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
     }
 }//Activity
