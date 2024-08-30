39662c8f7074d187b0073e4ff48581a7ca4c3672
helen-cho <hd9536@daum.net>
2024-08-30 11:20:21 +0900

    카카오

--- ex05/app/src/main/java/com/example/ex05/BlogFragment.java
+++ ex05/app/src/main/java/com/example/ex05/BlogFragment.java
@@ -3,15 +3,19 @@ package com.example.ex05;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.text.Html;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -22,11 +26,11 @@ import org.json.JSONObject;
 public class BlogFragment extends Fragment {
     JSONArray array=new JSONArray();
     BlogAdapter adapter=new BlogAdapter();
-
+    String query="와인";
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
+        setHasOptionsMenu(true);
         View view=inflater.inflate(R.layout.fragment_blog, container, false);
         new BlogThread().execute();
         ListView list=view.findViewById(R.id.list);
@@ -37,7 +41,7 @@ public class BlogFragment extends Fragment {
     class BlogThread extends AsyncTask<String, String, String>{
         @Override
         protected String doInBackground(String... strings) {
-            String url="https://dapi.kakao.com/v2/search/blog?query=커피";
+            String url="https://dapi.kakao.com/v2/search/blog?query=" + query;
             String result=KakaoAPI.connect(url);
             return result;
         }
@@ -84,5 +88,25 @@ public class BlogFragment extends Fragment {
             }
             return item;
         }
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.main, menu);
+        SearchView searchView=(SearchView)menu.findItem(R.id.search).getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                query=s;
+                new BlogThread().execute();
+                return false;
+            }
+        });
+        super.onCreateOptionsMenu(menu, inflater);
     }
 }//Flagment
