235b2f026342291e0c0df23d13f604d97c9eecad
helen-cho <hd9536@daum.net>
2024-08-30 10:22:30 +0900

    카카오

--- ex05/app/src/main/java/com/example/ex05/MainActivity.java
+++ ex05/app/src/main/java/com/example/ex05/MainActivity.java
@@ -11,10 +11,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
     DrawerLayout drawerLayout;
     LinearLayout drawerView;
+    TabLayout tab;
+    ViewPager pager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +31,16 @@ public class MainActivity extends AppCompatActivity {
 
         drawerLayout=findViewById(R.id.drawerLayout);
         drawerView=findViewById(R.id.drawerView);
+
+        tab=findViewById(R.id.tab);
+        pager=findViewById(R.id.pager);
+
+        tab.addTab(tab.newTab().setText("블로그"));
+        tab.getTabAt(0).setIcon(R.drawable.blog);
+        tab.addTab(tab.newTab().setText("도서"));
+        tab.getTabAt(1).setIcon(R.drawable.book);
+        tab.addTab(tab.newTab().setText("지역"));
+        tab.getTabAt(2).setIcon(R.drawable.local);
     }
 
     @Override
