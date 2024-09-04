aad825154f89273f64e9666216f881ba328b9a86
helen-cho <hd9536@daum.net>
2024-09-04 13:14:13 +0900

    홈

--- ex08/app/src/main/java/com/example/ex08/HomeFragment.java
+++ ex08/app/src/main/java/com/example/ex08/HomeFragment.java
@@ -1,64 +1,134 @@
 package com.example.ex08;
 
+import static com.example.ex08.RemoteService.BASE_URL;
+
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import java.util.HashMap;
+
 public class HomeFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public HomeFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    Retrofit retrofit;
+    RemoteService remoteService;
+    int page=1;
+    int total=0;
+    JSONArray array=new JSONArray();
+    WineAdapter adapter=new WineAdapter();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view=inflater.inflate(R.layout.fragment_home, container, false);
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        remoteService=retrofit.create(RemoteService.class);
+        getList();
+
+        RecyclerView list=view.findViewById(R.id.list);
+        list.setAdapter(adapter);
+        StaggeredGridLayoutManager manager=
+                new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.HORIZONTAL);
+        list.setLayoutManager(manager);
+        return view;
+    }//onCreateView
+
+    public void getList(){
+        Call<HashMap<String,Object>> call=remoteService.list(page);
+        call.enqueue(new Callback<HashMap<String, Object>>() {
+            @Override
+            public void onResponse(Call<HashMap<String, Object>> call, Response<HashMap<String, Object>> response) {
+                JSONObject object=new JSONObject(response.body());
+                try {
+                    total = object.getInt("total");
+                    array = object.getJSONArray("list");
+                    Log.i("total", total + "");
+                    Log.i("length", array.length() + "");
+                    adapter.notifyDataSetChanged();
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<HashMap<String, Object>> call, Throwable t) {
+
+            }
+        });
+    }//getList()
+
+    class WineAdapter extends RecyclerView.Adapter<WineAdapter.ViewHolder>{
+        @NonNull
+        @Override
+        public WineAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View item = getLayoutInflater().inflate(R.layout.item_wine,parent,false);
+            return new ViewHolder(item);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull WineAdapter.ViewHolder holder, int position) {
+            try {
+                JSONObject obj=array.getJSONObject(position);
+                String image=obj.getString("wine_image");
+                int index = obj.getInt("index");
+                Picasso.with(getActivity()).load(image).into(holder.image);
+                holder.index.setText(String.valueOf(index));
+                float rating=Float.parseFloat(obj.getString("rating"));
+                holder.rating.setRating(rating);
+                String country=obj.getString("wine_country");
+                holder.country.setText(country);
+                String type=obj.getString("wine_type");
+                holder.type.setText(type);
+                String name=obj.getString("wine_name");
+                holder.name.setText(name);
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return array.length();
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder{
+            ImageView image;
+            TextView name, type, country, price, index;
+            RatingBar rating;
+            public ViewHolder(@NonNull View itemView) {
+                super(itemView);
+                image=itemView.findViewById(R.id.image);
+                name=itemView.findViewById(R.id.name);
+                type=itemView.findViewById(R.id.type);
+                country=itemView.findViewById(R.id.country);
+                price=itemView.findViewById(R.id.price);
+                rating=itemView.findViewById(R.id.rating);
+                index=itemView.findViewById(R.id.index);
+            }
+        }
     }
-}
+}//Fragment
